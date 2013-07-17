e0e0ed500ee442297315bbfd3dae4ba3c65eb771
kolabell <hongyemosuo@hotmail.com>
2013-07-17 17:54:33 +0800

    init basic yii code

---
+++ protected/yiic.php
@@ -0,0 +1,7 @@
+<?php
+
+// change the following paths if necessary
+$yiic=dirname(__FILE__).'/../../yii-1.1.14-rc.2d9982/framework/yiic.php';
+$config=dirname(__FILE__).'/config/console.php';
+
+require_once($yiic);
