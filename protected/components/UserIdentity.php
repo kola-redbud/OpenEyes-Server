2b7a37bea2de56acd310d143ffe4c7b4ae839836
kolabell <hongyemosuo@hotmail.com>
2013-07-20 16:56:43 +0800

    milestone 1

--- protected/components/UserIdentity.php
+++ protected/components/UserIdentity.php
@@ -7,6 +7,7 @@
  */
 class UserIdentity extends CUserIdentity
 {
+    private $_id;
 	/**
 	 * Authenticates a user.
 	 * The example implementation makes sure if the username and password
@@ -17,6 +18,19 @@ class UserIdentity extends CUserIdentity
 	 */
 	public function authenticate()
 	{
+        $username = strtolower($this->username);
+        $user = User::model()->find('LOWER(username)=?',array($username));
+        if($user === null)
+            $this->errorCode = self::ERROR_USERNAME_INVALID;
+        else if(!$user->validatePassword($this->password))
+            $this->errorCode = self::ERROR_PASSWORD_INVALID;
+        else{
+            $this->_id = $user->id;
+            $this->username = $user->username;
+            $this->errorCode = self::ERROR_NONE;
+        }
+        return $this->errorCode == self::ERROR_NONE;
+        /*
 		$users=array(
 			// username => password
 			'demo'=>'demo',
@@ -29,5 +43,10 @@ class UserIdentity extends CUserIdentity
 		else
 			$this->errorCode=self::ERROR_NONE;
 		return !$this->errorCode;
+        */
 	}
+
+    public function getId(){
+        return $this->_id;
+    }
 }
